c7f6cae6f594786914bdab07997ad7a8b865f59d
Eka2023 <135666872+Eka2023@users.noreply.github.com>
2023-06-23 16:34:51 -0400

    additional DropDownTests were added

--- src/main/java/pageobjects/SignUpPage.java
+++ src/main/java/pageobjects/SignUpPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,14 +18,42 @@ public class SignUpPage extends BaseMain {
 
     By jobTitleList = By.id("job-title");
     By jobTitle = By.xpath("//select[@id='job-title']//option");
+
     public List<String> getJobTitleList() {
         WebElement dropDownElement = driver.findElement(jobTitleList);
         Select searchDropDown = new Select(dropDownElement);
         List<WebElement> list = searchDropDown.getOptions();
         List<String> resultList = new ArrayList<>();
-        for(int i=0; i< list.size(); i++){
+        for (int i = 0; i < list.size()-1; i++) {
             resultList.add(list.get(i).getText());
-            System.out.println(list.get(i).getText());
+            //System.out.println(list.get(i).getText());
+        }
+        return resultList;
+    }
+
+    public List<String> getJobListWithPreferableTitle(String title) {
+        List<String> allTitleList = getJobTitleList();
+        List<String> resultList = new ArrayList<>();
+        for (int i = 0; i < allTitleList.size(); i++) {
+            String tmp = allTitleList.get(i);
+            if (tmp.toLowerCase().contains(title.toLowerCase())) {
+                resultList.add(tmp);
+                System.out.println(tmp);
+            }
+        }
+
+        return resultList;
+    }
+
+    public List<String> getJobListWithPreferableTitle(String title, String title1) {
+        List<String> allTitleList = getJobTitleList();
+        List<String> resultList = new ArrayList<>();
+        for (int i = 0; i < allTitleList.size()-1; i++) {
+            String tmp = allTitleList.get(i);
+            if (tmp.toLowerCase().contains(title.toLowerCase())
+                    || tmp.toLowerCase().contains(title1.toLowerCase())) {
+                resultList.add(tmp);
+            }
         }
         return resultList;
     }
@@ -32,23 +61,39 @@ public class SignUpPage extends BaseMain {
     public List<String> getJobTitleListWithoutSeleniumSelect() {
         List<WebElement> elementsList = driver.findElements(jobTitle);
         List<String> resultList = new ArrayList<>();
-        for(int i=0; i< elementsList.size(); i++){
+        for (int i = 0; i < elementsList.size()-1; i++) {
             resultList.add(elementsList.get(i).getText());
             System.out.println(elementsList.get(i).getText());
         }
         return resultList;
     }
 
-    public List<String> expectedJobTitleList(){
-        List<String> expectedList = Arrays.asList("Junior level Developer", "Middle level Developer", "Senior level Developer", "Leading Developer", "QA Analyst", "Software Tester", "QA Engineer", "Senior QA Engineer", "Automation QA Engineer", "Test Lead", "Automation Test Lead", "Business Analyst", "Senior Business Analyst", "Designer");
+    public List<String> expectedJobTitleList() {
+        List<String> expectedList = Arrays.asList("Junior level Developer", "Middle level Developer", "Senior level Developer", "Leading Developer", "QA Analyst, Software Tester", "QA Engineer", "Senior QA Engineer", "Automation QA Engineer", "Test Lead", "Automation Test Lead", "Business Analyst", "Senior Business Analyst", "Designer");
         return expectedList;
     }
 
-    public boolean assertActualAndExpectedList(List<String> actual, List<String> expected){
-        boolean equals;
-        if(actual.equals(expected)){
-            equals = true;
-        }else equals = false;
-        return equals;
+    public List<String> expectedDevJobTitleList() {
+        List<String> expectedList = Arrays.asList("Junior level Developer", "Middle level Developer", "Senior level Developer", "Leading Developer");
+        return expectedList;
+    }
+
+    public List<String> expectedQAJobTitleList() {
+        List<String> expectedList = Arrays.asList("QA Analyst, Software Tester", "QA Engineer", "Senior QA Engineer", "Automation QA Engineer", "Test Lead", "Automation Test Lead");
+        return expectedList;
+    }
+
+    public List<String> expectedAnalystJobTitleList() {
+        List<String> expectedList = Arrays.asList("QA Analyst, Software Tester", "Business Analyst", "Senior Business Analyst");
+        return expectedList;
+    }
+
+    public List<String> expectedBAJobTitleList() {
+        List<String> expectedList = Arrays.asList("Business Analyst", "Senior Business Analyst");
+        return expectedList;
+    }
+
+    public void assertActualAndExpectedList(List<String> actual, List<String> expected) {
+        Assert.assertEquals(expected,actual);
     }
 }
--- src/test/java/testcases/DropdownTests.java
+++ src/test/java/testcases/DropdownTests.java
@@ -13,6 +13,37 @@ public class DropdownTests extends BaseTest{
     }
 
     @Test
+    public void printDevJobTitleList() {
+        homePage.clickSignUpBtn();
+        signUpPage.assertActualAndExpectedList(
+                signUpPage.getJobListWithPreferableTitle("developer"),
+                signUpPage.expectedDevJobTitleList());
+    }
+
+    @Test
+    public void printQAJobTitleList() {
+        homePage.clickSignUpBtn();
+        signUpPage.assertActualAndExpectedList(
+                signUpPage.getJobListWithPreferableTitle("test", "QA"),
+                signUpPage.expectedQAJobTitleList());
+    }
+
+    @Test
+    public void printAnalystJobTitleList() {
+        homePage.clickSignUpBtn();
+        signUpPage.assertActualAndExpectedList(
+                signUpPage.getJobListWithPreferableTitle("analyst"),
+                signUpPage.expectedAnalystJobTitleList());
+    }
+
+    @Test
+    public void printBAJobTitleList() {
+        homePage.clickSignUpBtn();
+        signUpPage.assertActualAndExpectedList(
+                signUpPage.getJobListWithPreferableTitle("business"),
+                signUpPage.expectedBAJobTitleList());
+    }
+    @Test
     public void printAllJobTitleWithoutSeleniumSelection() {
         homePage.clickSignUpBtn();
         signUpPage.assertActualAndExpectedList(
